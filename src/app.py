b90e27c8491fb434218c9ec88af0aae82cde98c6
Mia <mm2632@cornell.edu>
2023-11-13 20:14:30 -0500

    Add possible routes

--- src/app.py
+++ src/app.py
@@ -13,8 +13,25 @@ with app.app_context():
     db.create_all()
 
 
-# your routes here
+# FOR HOME PAGE:
+# @app.route("/api/applications/")
+# GET all applications
+    # in this one we look through each one and see if deadline passed to call DELETE
 
+# FOR FILTERING:
+# @app.route("/api/applications/<int:category_id>/")
+# GET all applications by category id
+    # I have a feeling the iOS people would input the category string (not category_id)
+        # so wondering if we should also have a GET for getting category name -> category id?
+        # or replace category_id by a category string (so that the front end can access it that way)
+
+# ADD APPLICATION:
+# @app.route("/api/applications/", methods=["POST"])
+# POST an application
+
+# DELETE APPLICATION (past due date):
+# @app.route("/api/applications/<int:application_id>/", methods=["DELETE"])
+# DELETE an application by id
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
